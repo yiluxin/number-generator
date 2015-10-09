c4c788eee8889c54b6ccfcd77297160cb07f3d0f
zjy <irrigator@yeah.net>
2015-10-10 00:17:27 +0800

    Store exported number pairs in localStorage

--- App.jsx
+++ App.jsx
@@ -1,5 +1,47 @@
 const maxLength = 11;
 
+if (!String.prototype.repeat) {
+  String.prototype.repeat = function(count) {
+    'use strict';
+    if (this == null) {
+      throw new TypeError('can\'t convert ' + this + ' to object');
+    }
+    var str = '' + this;
+    count = +count;
+    if (count != count) {
+      count = 0;
+    }
+    if (count < 0) {
+      throw new RangeError('repeat count must be non-negative');
+    }
+    if (count == Infinity) {
+      throw new RangeError('repeat count must be less than infinity');
+    }
+    count = Math.floor(count);
+    if (str.length == 0 || count == 0) {
+      return '';
+    }
+    // Ensuring count is a 31-bit integer allows us to heavily optimize the
+    // main part. But anyway, most current (August 2014) browsers can't handle
+    // strings 1 << 28 chars or longer, so:
+    if (str.length * count >= 1 << 28) {
+      throw new RangeError('repeat count must not overflow maximum string size');
+    }
+    var rpt = '';
+    for (;;) {
+      if ((count & 1) == 1) {
+        rpt += str;
+      }
+      count >>>= 1;
+      if (count == 0) {
+        break;
+      }
+      str += str;
+    }
+    return rpt;
+  }
+}
+
 App = React.createClass({
 
   getInitialState() {
@@ -41,7 +83,7 @@ App = React.createClass({
   startNumberInputStatus() {
     return this.state.startNumber.length < 3 ?
       '至少还需要输入' + (3 - this.state.startNumber.length) + '位数字' :
-      '还能输入' + (this.startNumberMaxLength() - this.state.startNumber.length) + '位数字';
+        '还能输入' + (this.startNumberMaxLength() - this.state.startNumber.length) + '位数字';
   },
 
   endNumberMaxLength() {
@@ -51,7 +93,7 @@ App = React.createClass({
   endNumberInputStatus() {
     return this.state.endNumber.length < 3 ?
       '至少还需要输入' + (3 - this.state.endNumber.length) + '位数字' :
-      '还能输入' + (this.endNumberMaxLength() - this.state.endNumber.length) + '位数字';
+        '还能输入' + (this.endNumberMaxLength() - this.state.endNumber.length) + '位数字';
   },
 
   showNumberPairs() {
@@ -68,11 +110,11 @@ App = React.createClass({
     let nextNumberPairs = this.state.numberPairs.concat({start: this.state.startNumber, end: this.state.endNumber})
     this.setState({ numberPairs: nextNumberPairs });
     window.localStorage.setItem('numberPairs', JSON.stringify(nextNumberPairs));
-    
+
   },
 
   numberPairHasBeenExported() {
-    return this.state.numberPairs.some(numberPair => {
+    return _.some(this.state.numberPairs, numberPair => {
       return this.state.startNumber === numberPair.start && this.state.endNumber === numberPair.end;
     });
   },
@@ -87,39 +129,43 @@ App = React.createClass({
               {this.showNumberPairs()}
             </ul>
           </div>
-        ) : ''}
+          ) : ''}
 
-        <input
-          type="text"
-          ref="startNumber"
-          placeholder="输入开头数字"
-          maxLength={this.startNumberMaxLength()}
-          onChange={this.setStartNumber}
-        />
-        <span>{this.startNumberInputStatus()}</span>
+          <input
+            type="text"
+            ref="startNumber"
+            placeholder="输入开头数字"
+            maxLength={this.startNumberMaxLength()}
+            onChange={this.setStartNumber}
+          />
+          <span>{this.startNumberInputStatus()}</span>
 
-        <br />
+          <br />
 
-        <input
-          type="text"
-          ref="endNumber"
-          placeholder="输入结尾数字"
-          maxLength={this.endNumberMaxLength()}
-          onChange={this.setEndNumber}
-        />
-        <span>{this.endNumberInputStatus()}</span>
+          <input
+            type="text"
+            ref="endNumber"
+            placeholder="输入结尾数字"
+            maxLength={this.endNumberMaxLength()}
+            onChange={this.setEndNumber}
+          />
+          <span>{this.endNumberInputStatus()}</span>
 
-        <br />
+          <br />
 
-        <span>中间数字长度：{maxLength - this.state.startNumber.length - this.state.endNumber.length}</span>
+          <span>中间数字长度：{maxLength - this.state.startNumber.length - this.state.endNumber.length}</span>
 
-        {this.numberPairHasBeenExported() ?
-          <span>已导入过该号码组合</span> :
-          <button onClick={this.exportToContacts}>导入到通讯录</button>
-          }
+          <br />
+
+          {this.numberPairHasBeenExported() ?
+            <span>已导入过该号码组合</span> :
+              this.state.startNumber.length > 2 && this.state.endNumber.length > 2 ?
+                <button onClick={this.exportToContacts}>导入到通讯录</button> :
+                  <span>至少需要输入3位开头数字和3位结尾数字</span>
+                  }
 
 
-      </div>
+                </div>
     )
   }
 });
