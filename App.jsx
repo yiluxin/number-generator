e4af3581a7425a6388d0f1909a7acccbfec39505
Illusion <779802117@qq.com>
2015-10-13 21:51:42 +0800

    Revert "add some style"

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 const maxLength = 11;
+
 if (!String.prototype.repeat) {
-  String.prototype.repeat = function (count) {
+  String.prototype.repeat = function(count) {
     'use strict';
     if (this == null) {
       throw new TypeError('can\'t convert ' + this + ' to object');
@@ -27,7 +28,7 @@ if (!String.prototype.repeat) {
       throw new RangeError('repeat count must not overflow maximum string size');
     }
     var rpt = '';
-    for (; ;) {
+    for (;;) {
       if ((count & 1) == 1) {
         rpt += str;
       }
@@ -60,7 +61,7 @@ App = React.createClass({
 
   componentWillMount() {
     let numberPairs = window.localStorage.getItem('numberPairs');
-    this.setState({numberPairs: JSON.parse(numberPairs)});
+    this.setState({ numberPairs: JSON.parse(numberPairs) });
   },
 
   setStartNumber(event) {
@@ -89,8 +90,8 @@ App = React.createClass({
 
   startNumberInputStatus() {
     return this.state.startNumber.length < 3 ?
-    '开头至少还需要输入' + (3 - this.state.startNumber.length) + '位数字' :
-    '开头还能输入' + (this.startNumberMaxLength() - this.state.startNumber.length) + '位数字';
+      '至少还需要输入' + (3 - this.state.startNumber.length) + '位数字' :
+        '还能输入' + (this.startNumberMaxLength() - this.state.startNumber.length) + '位数字';
   },
 
   endNumberMaxLength() {
@@ -99,8 +100,8 @@ App = React.createClass({
 
   endNumberInputStatus() {
     return this.state.endNumber.length < 3 ?
-    '后缀至少还需要输入' + (3 - this.state.endNumber.length) + '位数字' :
-    '后缀还能输入' + (this.endNumberMaxLength() - this.state.endNumber.length) + '位数字';
+      '至少还需要输入' + (3 - this.state.endNumber.length) + '位数字' :
+        '还能输入' + (this.endNumberMaxLength() - this.state.endNumber.length) + '位数字';
   },
 
   showNumberPairs() {
@@ -108,29 +109,17 @@ App = React.createClass({
       let {start, end} = numberPair;
       let middle = 'x'.repeat(maxLength - start.length - end.length);
       let result = start + middle + end;
-      return <li className="list-group-item" key={result}>{result}</li>
+      return <li key={result}>{result}</li>
     });
   },
 
   exportToContacts() {
-    if (this.numberPairHasBeenExported()) {
-      alert("已导入过该号码组合");
-      return
-    }
-    if (this.state.startNumber.length < 2 && this.state.endNumber.length < 2) {
-      alert("需要输入3位开头数字和3位结尾数字");
-      return
-    }
-
     let start = this.state.startNumber;
     let end = this.state.endNumber;
-    let length = maxLength - start.length - end.length;
+    let length = maxLength - start.length - end.length
     // 更新numberPairs (state & localStorage)
-    let nextNumberPairs = (this.state.numberPairs && this.state.numberPairs.concat({
-          start: start,
-          end: end
-        })) || [{start: start, end: end}];
-    this.setState({numberPairs: nextNumberPairs});
+    let nextNumberPairs = (this.state.numberPairs && this.state.numberPairs.concat({start: start, end: end})) || [{start: start, end: end}];
+    this.setState({ numberPairs: nextNumberPairs });
     window.localStorage.setItem('numberPairs', JSON.stringify(nextNumberPairs));
 
     // actually export numbers to contacts, might take a long time
@@ -138,9 +127,7 @@ App = React.createClass({
       let number = start + zeroPad(i, length) + end;
       let phoneNumbers = [];
       phoneNumbers[0] = new ContactField('mobile', number, true); // preferred number
-      navigator.contacts.create({"phoneNumbers": phoneNumbers}).save(function () {
-        alert('ok')
-      });
+      navigator.contacts.create({"phoneNumbers": phoneNumbers}).save();
     }
 
 
@@ -153,44 +140,50 @@ App = React.createClass({
   },
 
   render() {
-    var style = {};
-    style.center = {textAlign: 'center'};
     return (
-        <div className="container">
-          <h2 style={style.center}>编号神器</h2>
-
-          <p className="color-gainsboro" style={style.center}>{this.startNumberInputStatus()}</p>
-          <input className="form-control input-lg marginBottom"
-                 type="number"
-                 ref="startNumber"
-                 placeholder="输入开头数字"
-                 maxLength={this.startNumberMaxLength()}
-                 onChange={this.setStartNumber}/>
-
-          <p className="color-gainsboro" style={style.center}>{this.endNumberInputStatus()}</p>
-          <input
-              className="form-control input-lg marginBottom"
-              type="number"
-              ref="endNumber"
-              placeholder="输入结尾数字"
-              maxLength={this.endNumberMaxLength()}
-              onChange={this.setEndNumber}
-              />
-
-          <div className="color-gainsboro marginBottom" style={style.center}>
-            中间数字长度：{maxLength - this.state.startNumber.length - this.state.endNumber.length}
+      <div className="container">
+        {this.state.numberPairs ? (
+          <div className="number-pairs">
+            <h2>已导入号码字段</h2>
+            <ul>
+              {this.showNumberPairs()}
+            </ul>
           </div>
-
-          <button className="btn btn-primary btn-block btn-lg marginBottom" onClick={this.exportToContacts}>
-            导入到通讯录
-          </button>
-          {this.state.numberPairs ? (
-              <ul className="list-group">
-                <li className="list-group-item disabled">已导入号码字段</li>
-                {this.showNumberPairs()}
-              </ul>
           ) : ''}
-        </div>
+
+          <input
+            type="text"
+            ref="startNumber"
+            placeholder="输入开头数字"
+            maxLength={this.startNumberMaxLength()}
+            onChange={this.setStartNumber}
+          />
+          <span>{this.startNumberInputStatus()}</span>
+
+          <br />
+
+          <input
+            type="text"
+            ref="endNumber"
+            placeholder="输入结尾数字"
+            maxLength={this.endNumberMaxLength()}
+            onChange={this.setEndNumber}
+          />
+          <span>{this.endNumberInputStatus()}</span>
+
+          <br />
+
+          <span>中间数字长度：{maxLength - this.state.startNumber.length - this.state.endNumber.length}</span>
+
+          {this.numberPairHasBeenExported() ?
+            <span>已导入过该号码组合</span> :
+              this.state.startNumber.length > 2 && this.state.endNumber.length > 2 ?
+                <button onClick={this.exportToContacts}>导入到通讯录</button> :
+                  <span>至少需要输入3位开头数字和3位结尾数字</span>
+                  }
+
+
+                </div>
     )
   }
 });
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Number Generator
 
 A number generator, built with Meteor, Cordova and React.
+
